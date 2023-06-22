2f61afd171b09e91215ad3e477caf4465294f9ca
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-06-22 13:18:09 +0300

    betty fix

--- Readfile.c
+++ Readfile.c
@@ -1,6 +1,11 @@
 #include "monty.h"
 #include <string.h>
-
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: 0(success)
+ */
 int main(int argc, char *argv[])
 {
 	int fd;
@@ -18,12 +23,12 @@ int main(int argc, char *argv[])
 	fd = open(argv[1], O_RDONLY);
 	f = fdopen(fd, "r");
 	if (fd == -1 || f == NULL)
-        {
+	{
 		if (fd != -1)
 			close(fd); 
-                fprintf(stderr,"Error: Can't open file %s\n", argv[1]);
-                exit(EXIT_FAILURE);
-        }
+		fprintf(stderr,"Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 	while (getline(&lineptr, &n, f) != -1)
 	{
 		str1 = _strtok(lineptr);
@@ -39,8 +44,15 @@ int main(int argc, char *argv[])
 	free_stack(top);
 	free(lineptr);
 	fclose(f);
-	return(0);
+	return (0);
 }
+/**
+ * search_function - searches a function and calles it
+ * @stack: top of the stack
+ * @str1: tokinized strings
+ * @line_number: number of the line
+ * Return: nothing
+ */
 void search_function(stack_t **stack, char **str1, unsigned int line_number)
 {
 	void (*fun)(stack_t **stack, unsigned int line_number);
@@ -61,8 +73,6 @@ void search_function(stack_t **stack, char **str1, unsigned int line_number)
 	if (fun != NULL)
 		fun(stack, line_number);
 }
-
-
 /**
  * free_maloc - frees pointer to pointer
  * dynamic allocation
